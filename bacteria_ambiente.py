b6c789e4a84adf279d6014138eaabf1aa3c0c97e
esbiten-hub <estebustamante990@gmail.com>
2025-06-12 22:33:07 -0400

    se establecen valores de inicio

--- bacteria_ambiente.py
+++ bacteria_ambiente.py
@@ -1,13 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Patch
+
 class Bacteria:
-    def __init__(self, id, raza, energia, resistencia):
+    def __init__(self, id, raza):
         self.__id = id
         self.__raza = raza
-        self.__energia = energia
-        self.__resistencia = resistencia
+        self.__energia = 100
+        self.__resistencia = False
         self.__estado = 'activa'
     
     def get_id(self):
-        
         return self.__id
     
     def set_id(self, id):
@@ -55,6 +58,18 @@ class Bacteria:
         except ValueError as e:
             print(f"Error: {e}")
     
+    def get_estado(self):
+        return self.__estado
+
+    def set_estado(self, estado):
+        try:
+            if isinstance(estado, str):
+                self.__estado = estado
+            else:
+                raise ValueError("El estado debe ser una cadena de texto.")
+        except ValueError as e:
+            print(f"Error: {e}")
+    
     def alimentar(self):
         pass
 
@@ -69,22 +84,51 @@ class Bacteria:
         pass
 
 class Ambiente:
-    def __init__(self, grilla, nutrientes, factor_ambiental):
-        self.__grilla = grilla
-        self.__nutrientes = nutrientes
+    def __init__(self, factor_ambiental):
+        self.__grilla = set_grilla()
+        self.__nutrientes = 50
         self.__factor_ambiental = factor_ambiental
     
     def get_grilla(self):
         return self.__grilla
     
-    def set_grilla(self, grilla):
-        try:
-            if isinstance(grilla, ndarray):
-                self.__grilla = grilla
-            else:
-                raise ValueError("La grilla debe ser un ndarray.")
-        except ValueError as e:
-            print(f"Error: {e}")
+    def set_grilla(self):
+
+        #Crea una grilla 50x50
+        grilla = np.zeros((50, 50))
+
+        #Crea mapa de colores con 5 categorías
+        cmap = plt.cm.get_cmap('Set1', 5)
+
+        fig, ax = plt.subplots(figsize=(6,6))
+        cax = ax.matshow(grilla, cmap=cmap)
+
+        #Agrega leyenda
+        legend_elements = [
+            Patch(facecolor = cmap (1/5), label = 'Bacteria activa'),
+            Patch(facecolor = cmap (2/5), label = 'Bacteria muerta'),
+            Patch(facecolor = cmap (3/5), label = 'Bacteria resistente'),
+            Patch(facecolor = cmap (4/5), label = 'Biofilm'),
+        ]
+
+        ax.legend(handless = legend_elements, loc = 'upper right', bbox_to_anchor =  (1.45, 1))
+
+        #Configuracion de la grilla
+        ax.set_xticks(np.arange(0, 50, 1))
+        ax.set_yticks(np.arange(0, 50, 1))
+        ax.set_xticklabels([])
+        ax.set_ytickslabels([])
+        ax.grid(color = 'gray', linestyle = '-', linewidth = 0.5)
+
+        for i in range(50):
+            for j in range(50):
+                val = grilla [i, j]
+                if val > 0:
+                    ax.text(j, i, int(val), va = 'center', ha = 'center', color = 'white')
+
+        plt.title("Colonia de bacterias")
+        plt.tight_layout()
+        plt.show()
 
     def get_nutrientes(self):
         return self.__nutrientes

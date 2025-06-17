8353dcea8d6f9bb076b85d46f010dd87b99afc47
esbiten-hub <estebustamante990@gmail.com>
2025-06-17 00:50:40 -0400

    return bytes a main

--- simulador.py
+++ simulador.py
@@ -1,13 +1,15 @@
+import io
+from PIL import Image
 from colonia import Colonia
 from bacteria_ambiente import Bacteria, Ambiente
 
 class Simulador():
     def __init__(self, especie_bacteria, cantidad_bacterias, factor_ambiental, pasos):
-        self.especie_bacteria = especie_bacteria
-        self.cantidad_bacterias = cantidad_bacterias
-        self.factor_ambiental = factor_ambiental
-        self.pasos = pasos
-        self.colonia = None
+        self.especie_bacteria = especie_bacteria #str
+        self.cantidad_bacterias = cantidad_bacterias #int
+        self.factor_ambiental = factor_ambiental #str
+        self.pasos = pasos #int
+        self.colonia = None #Class Colonia
     
     def inicia_simulacion(self):
         lista_bacterias = []
@@ -19,7 +21,8 @@ class Simulador():
 
     def run(self):
         for i in range(self.pasos):
-            self.colonia.paso()
+            buf = self.colonia.paso(i)
+            return buf
 
     def graficar_crecimiento(self):
         pass
